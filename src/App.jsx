c15ed1ac3826ab14c5c64cb914eecbc8a260848b
Shcoobz <a.boeckmann23@gmail.com>
2024-05-13 20:54:51 +0200

    created game

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,9 @@
-import { useState } from 'react';
-import Header from './components/Header';
-import PlayerContainer from './components/PlayerContainer';
-import ComputerContainer from './components/ComputerContainer';
-import ResultRule from './components/ResultRule';
-import ResultText from './components/ResultText';
-import { startConfetti, stopConfetti, removeConfetti } from './confetti';
+import { rules, choices, computerRandomChoice, displayRule } from './game';
 
 function App() {
-  const [scores, setScores] = useState({ player: 0, computer: 0 });
-  const [playerChoice, setPlayerChoice] = useState('');
-  const [computerChoice, setComputerChoice] = useState('');
-  const [resultRule, setResultRule] = useState('');
-  const [resultText, setResultText] = useState('');
-
-  const resetAll = () => {
-    setScores({ player: 0, computer: 0 });
-    setPlayerChoice('');
-    setComputerChoice('');
-    setResultRule('');
-    setResultText('');
-    stopConfetti();
-    removeConfetti();
-  };
-
-
-
-
-
   return (
-    <div className='game-container'>
-      <Header resetAll={resetAll} />
+    <div className='App'>
+      <h1>Hello</h1>
     </div>
   );
 }
---
+++ src/game.js
@@ -0,0 +1,41 @@
+const rules = {
+  scissors: { paper: 'cuts', lizard: 'decapitates' },
+  paper: { rock: 'covers', spock: 'disproves' },
+  rock: { lizard: 'crushes', scissors: 'crushes' },
+  lizard: { spock: 'poisons', paper: 'eats' },
+  spock: { scissors: 'smashes', rock: 'vaporizes' },
+};
+
+const choices = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
+
+function computerRandomChoice() {
+  const choiceIndex = Math.floor(Math.random() * choices.length);
+  return choices[choiceIndex];
+}
+
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+function displayRule(playerChoice, computerChoice) {
+  const playerWinningRule = rules[playerChoice]?.[computerChoice];
+  const computerWinningRule = rules[computerChoice]?.[playerChoice];
+
+  let ruleText = 'Nothing happens.';
+  if (playerWinningRule) {
+    ruleText = `<span class="player-color">${capitalizeFirstLetter(
+      playerChoice
+    )}</span> ${playerWinningRule} <span class="computer-color">${capitalizeFirstLetter(
+      computerChoice
+    )}</span>.`;
+  } else if (computerWinningRule) {
+    ruleText = `<span class="computer-color">${capitalizeFirstLetter(
+      computerChoice
+    )}</span> ${computerWinningRule} <span class="player-color">${capitalizeFirstLetter(
+      playerChoice
+    )}</span>.`;
+  }
+  return ruleText;
+}
+
+export { rules, choices, computerRandomChoice, displayRule };
